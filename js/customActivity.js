07fc7aa44818f5b7d2e997aa11b15896cbe0c969
Mark Mukherjee <mmukherjee@mmukherjee-ltm2.internal.salesforce.com>
2020-06-28 13:18:15 +0800

    updated front end html

--- js/customActivity.js
+++ js/customActivity.js
@@ -80,11 +80,13 @@ define([
                                         var arr_de_fields = json_response.de_fields;
                                         var txt1 = "";
                                         var i;
+                                        var j;
+                                        for(i=0; i < arr_de_fields.length; i++){
+                                            alert(arr_de_fields[0].Name);
+                                        }
 
-                                        alert(arr_de_fields[0].Name);
 
-
-                                        for (i = 0; i < 5; i++) {
+                                        for (j = 0; j < 5; j++) {
                                             txt1 += "<div class=\"activity-detail slds-grid slds-m-bottom_medium\"><div class=\"deupdate-attribute-list\"><div class=\"slds-grid\"><div class=\"deupdate-field-dropdown slds-col slds-size_5-of-12\"><div id=\"dropdownc109\"><div class=\"slds-combobox_container\"><div class=\"slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-combobox-picklist\" aria-expanded=\"false\" aria-haspopup=\"listbox\" role=\"combobox\"><div class=\"slds-combobox__form-element slds-input-has-icon slds-input-has-icon_right\" role=\"none\"><input type=\"text\" class=\"slds-input slds-combobox__input\" id=\"dropdownc109-input\" aria-controls=\"dropdownc109-input\" autocomplete=\"off\" role=\"textbox\" placeholder=\"Select an Attribute\" readonly=\"\" value=\"EmailAddress\"></div></div></div></div></div><div class=\"slds-col slds-size_1-of-12 slds-text-align_center equals-symbol\">=</div><div class=\"slds-form-element\"><div class=\"slds-form-element__control\"><div class=\"slds-select_container\"><select class=\"slds-select\" id=\"select-01\" required=\"\"><option value=\"\" disabled selected>Please select</option><option value=\"GenericUserEvent\">Field 1</option><option value=\"ProductPurchase\">Field 2</option></select></div></div></div></div></div></div>";
                                         }
 
