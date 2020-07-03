8cf86f8705d138024103f3d8deabc19b3f344456
Mark Mukherjee <mmukherjee@mmukherjee-ltm2.internal.salesforce.com>
2020-07-03 22:32:31 +0800

    updates to front end

--- js/customActivity.js
+++ js/customActivity.js
@@ -189,13 +189,13 @@ define([
                 }
 
                 if(key === 'is_event_mappings'){
-                    alert('key:val - ' + key + ': ' + val)
+                    alert('key:val - ' + key + ': ' + val);
 
                     $.each(key, function(key1, val1) {
-                        message = val;
+                        //message = val;
                         alert('key1:val1 - ' + key1 + ': ' + val1);
 
-                    }
+                    });
                 }
             });
         });
