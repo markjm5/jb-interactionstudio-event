a8cc974d56dbe161e6584d70787abdbff5890ecb
Mark Mukherjee <mmukherjee@mmukherjee-ltm2.internal.salesforce.com>
2020-07-19 22:38:03 +0800

    updates to front end

--- js/customActivity.js
+++ js/customActivity.js
@@ -130,7 +130,7 @@ define([
         //$("#message").html(JSON.stringify(inArguments));
         //$("#message").html("1. Enter the Customer Key of the Entry Data Extension<br>2. Select an IS Template<br>3. Click SAVE EVENT SETTINGS<br>4. Click NEXT");
 
-        alert('init called:');
+        alert('Init Called');
 
         if(de_schema.length == 0){
             $("#message").html("Please go back and select an Entry Data Extension before proceeding further");
@@ -182,6 +182,10 @@ define([
             showStep(null, 1);
         }
     }
+
+    $( ".select-01" ).change(function() {
+        alert( "Handler for .change() called." );
+    });    
 
     function onGetTokens (tokens) {
         // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
