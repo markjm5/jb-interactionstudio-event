02f666163c843df5c7b3c878f340561eba9a040f
Mark Mukherjee <mmukherjee@mmukherjee-ltm2.internal.salesforce.com>
2020-07-19 13:28:11 +0800

    updates to front end

--- js/customActivity.js
+++ js/customActivity.js
@@ -409,7 +409,7 @@ define([
         for(i=0; i < arr_de_fields.length; i++){
 
             var val1 = arr_de_fields[i].Name;
-            var val2 = '{{Contact.' + de_name + '.' + val1 + '}}';
+            var val2 = '{{Contact.Attribute.' + de_name + '.' + val1 + '}}';
 
             //alert(val2);
 
